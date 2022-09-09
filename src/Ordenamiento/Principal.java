e1d9b33c6ce548d504fbbcc5563fd153991dbd6b
David Pat <picarofin@gmail.com>
2022-09-08 23:54:41 -0500

    Finalizacion del main y la interagion de guardar

--- src/Ordenamiento/Principal.java
+++ src/Ordenamiento/Principal.java
@@ -17,11 +17,14 @@ public class Principal {
         nombres= ModText.arreglarArray(nombres);
         nombres= OrdenamientoText.Ordenar(nombres);
 
+        System.out.println(">Nombres normalizados y ordenados");
         for(String s: nombres){
             System.out.println(s);
         }
+        System.out.println("--------------------");
 
-        
+        CreacionNArchivo.nuevoArchivoOrden(nombres);
+        System.out.println(">Archivo guardado exitosamente");  
     }
     
 }
