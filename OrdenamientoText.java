325d2530dd80fb68bcedfafdedc321579c70825a
Daniel Moreno <A20216399@alumnos.uady.mx>
2022-09-08 23:11:19 -0500

    Subida

--- OrdenamientoText.java
+++ OrdenamientoText.java
@@ -3,18 +3,21 @@ import java.util.Collections;
 
 public class OrdenamientoText{
     public static void main(String[] args) {
+        CreacionNArchivo arch=new CreacionNArchivo();
         OrdenamientoText llamarFunc=new OrdenamientoText();
         String nombre1="Pancho Villa";
         String nombre2="Daniel Tilla";
         String nombre3="Gancho Pilla";
-        ArrayList arrayNombres=new ArrayList<>();
+        ArrayList<String> arrayNombres=new ArrayList<String>();
         arrayNombres.add(nombre1);
         arrayNombres.add(nombre2);
         arrayNombres.add(nombre3);
         llamarFunc.OrdenarTxt(arrayNombres);
-        System.out.println(arrayNombres);
+        
+        arch.nuevoArchivoOrden(arrayNombres);
+
     }
-    public ArrayList OrdenarTxt(ArrayList<String> arrayNombres){
+    public ArrayList<String> OrdenarTxt(ArrayList<String> arrayNombres){
         Collections.sort(arrayNombres);
         return arrayNombres;
     }
