7869da1fba0bc52ccd2e095bbb6df02f5820dcc8
Daniel Moreno <A20216399@alumnos.uady.mx>
2022-09-08 22:26:40 -0500

    Func Ordenado

--- OrdenamientoText.java
+++ OrdenamientoText.java
@@ -1,17 +1,21 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class OrdenamientoText{
     public static void main(String[] args) {
+        OrdenamientoText llamarFunc=new OrdenamientoText();
         String nombre1="Pancho Villa";
         String nombre2="Daniel Tilla";
         String nombre3="Gancho Pilla";
-        ArrayList<String> arrayNombres=new ArrayList<>();
+        ArrayList arrayNombres=new ArrayList<>();
         arrayNombres.add(nombre1);
         arrayNombres.add(nombre2);
         arrayNombres.add(nombre3);
-        ((OrdenamientoText)arrayNombres).OrdenarTxt(arrayNombres);
+        llamarFunc.OrdenarTxt(arrayNombres);
+        System.out.println(arrayNombres);
     }
-    public void OrdenarTxt(ArrayList<String> arrayNombres){
-
+    public ArrayList OrdenarTxt(ArrayList<String> arrayNombres){
+        Collections.sort(arrayNombres);
+        return arrayNombres;
     }
 }
